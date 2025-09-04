8540654e49ca7fb19cb58b934e360fc75660bec4
Tttmods <travisvarnes79@gmail.com>
2025-09-03 23:13:28 -0400

    Delete ping.js

--- ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Latency",
-  async run({ message }) {
-    const m = await message.reply('Pinging...');
-    m.edit('🏓 Pong! ' + (m.createdTimestamp - message.createdTimestamp) + 'ms');
-  }
-};
