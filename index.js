85887c58c2f6067812b347767d48a35289cbc19e
Tttmods <travisvarnes79@gmail.com>
2025-09-03 23:13:00 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const path = require('path');
-const fs = require('fs');
-console.log('Starting NexaEverything...');
-// start bot and dashboard
-require('./bot')(process.env);
-require('./dashboard')(process.env);
